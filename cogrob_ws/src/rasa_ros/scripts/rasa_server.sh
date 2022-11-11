a8c5767e1bb2bbaf9f3be9065e1d95a0225531b9
giosullutrone <giovisull@gmail.com>
2022-11-11 17:02:41 +0100

    Testing .sh files

--- cogrob_ws/src/rasa_ros/scripts/rasa_server.sh
+++ cogrob_ws/src/rasa_ros/scripts/rasa_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-BOT_DIR="/home/colander/projects/cogrob/chatbot/"
-
-cd $BOT_DIR
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path"
+cd ../../../../chatbot
 
 rasa run -m models --endpoints endpoints.yml --port 5002 --credentials credentials.yml --enable-api

8b12e2369af95d8597164bbba7a04089f01055c6
giosullutrone <giovisull@gmail.com>
2022-12-01 21:07:30 +0100

    Added example folders and modified dialogue interface to integrate microphone node

--- cogrob_ws/src/rasa_ros/scripts/dialogue_interface.py
+++ cogrob_ws/src/rasa_ros/scripts/dialogue_interface.py
@@ -4,21 +4,28 @@ import rospy
 import warnings
 warnings.filterwarnings("ignore")
 from rasa_ros.srv import Dialogue, DialogueResponse
+from std_msgs.msg import String
+
 
 class TerminalInterface:
-    '''Class implementing a terminal i/o interface. 
+    '''
+    Class implementing a terminal i/o interface. 
 
     Methods
     - get_text(self): return a string read from the terminal
     - set_text(self, text): prints the text on the terminal
-
     '''
 
-    def get_text(self):
-        return input("[IN]:  ") 
+    def callback(self, data):
+        self.message = data
+        self.has_changed = True
 
-    def set_text(self,text):
-        print("[OUT]:",text)
+    def has_changed(self):
+        return self.has_changed
+
+    def get_text(self):
+        self.has_changed = False
+        return self.message
 
 def main():
     rospy.init_node('writing')
@@ -26,16 +33,18 @@ def main():
     dialogue_service = rospy.ServiceProxy('dialogue_server', Dialogue)
 
     terminal = TerminalInterface()
+    rospy.Subscriber("voice_txt", String, terminal.callback)
 
     while not rospy.is_shutdown():
-        message = terminal.get_text()
-        if message == 'exit': 
-            break
-        try:
-            bot_answer = dialogue_service(message)
-            terminal.set_text(bot_answer.answer)
-        except rospy.ServiceException as e:
-            print("Service call failed: %s"%e)
+        if terminal.has_changed():
+            message = terminal.get_text()
+            if message == 'exit': 
+                break
+            try:
+                bot_answer = dialogue_service(message)
+                terminal.set_text(bot_answer.answer)
+            except rospy.ServiceException as e:
+                print("Service call failed: %s"%e)
 
 if __name__ == '__main__':
     try: 

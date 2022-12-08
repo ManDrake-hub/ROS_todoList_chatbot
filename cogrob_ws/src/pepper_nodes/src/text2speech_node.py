dae34b49abe4fb475f02fb95243f7a16f21a134b
francesca2311 <f.tirino@studenti.unisa.it>
2022-12-08 11:59:58 +0000

    Move ALT2S/Event in voice_detection.py (mic) node NOT TESTED on Pepper

--- cogrob_ws/src/pepper_nodes/src/text2speech_node.py
+++ cogrob_ws/src/pepper_nodes/src/text2speech_node.py
@@ -7,7 +7,6 @@ from std_msgs.msg import String, Bool
 import time
 import qi
 
-tts_event_watcher = None
 '''
 This class implements a ROS node able to call the Text to speech service of the robot
 '''
@@ -16,13 +15,12 @@ class Text2SpeechNode:
     '''
     The costructor creates a session to Pepper and inizializes the services
     '''
-    def __init__(self, ip, port, pub):
+    def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.session = Session(ip, port)
         self.tts = self.session.get_service("ALTextToSpeech")
         self.tts.setLanguage("Italian")
-        self.pub = pub
      
     '''
     Rececives a Text2Speech message and call the ALTextToSpeech service.
@@ -31,12 +29,10 @@ class Text2SpeechNode:
     def say(self, msg):
         print("inizio il say ")
         try:
-            self.pub.publish(False)
             #MODIFICAAAAAAA METTI .DATA 
             self.tts.say(msg)
         except Exception as e:
             print(e)
-            print("mi riconnetto")
             self.session.reconnect()
             self.tts = self.session.get_service("ALTextToSpeech")
             self.tts.say(msg.data)
@@ -45,51 +41,21 @@ class Text2SpeechNode:
     Starts the node and create the tts service
     '''
     def start(self):
-        print("sono in start ")
         rospy.init_node("text2speech_node")
         self.say("ciao giovanni")
+        
+        ## DA DECOMMENTARE
         #rospy.Subscriber("bot_answer", String, self.say)
         rospy.spin()
 
-
-
-class SpeechWatcher:
-    """ A class to react to the ALTextToSpeech/Status event """
-
-    def __init__(self, app, pub):
-        app.start()
-        session = app.session
-        self.memory = session.service("ALMemory")
-        self.pub = pub
-        
-        self.subscriber = self.memory.subscriber("ALTextToSpeech/TextDone")
-        #print(dir(type(self.memory)))
-        #self.subscriber = self.memory.subscribeToEvent("ALTextToSpeech/TextDone", "text2speech_node", "on_text_done")
-
-        self.subscriber.signal.connect(self.on_text_done)
-
-        #self.pub = rospy.Publisher("/listen_start", String, queue_size =3)
-
-    def on_text_done(self, value):
-        print("oki")
-        if value == 1:
-            self.pub.publish(True)
-    
-
 if __name__ == "__main__":
-    print("Avvio nodo t2s")
     time.sleep(3)
     parser = OptionParser()
     parser.add_option("--ip", dest="ip", default="10.0.1.207")
     parser.add_option("--port", dest="port", default=9559)
     (options, args) = parser.parse_args()
-    pub = rospy.Publisher("check_listen", Bool, queue_size=10)
-    print("inizio il try")
     try:
-        ttsnode = Text2SpeechNode(options.ip, int(options.port), pub)
-        print("text2speech created ")
+        ttsnode = Text2SpeechNode(options.ip, int(options.port))
         ttsnode.start()
-        app = qi.Application(["SpeechWatcher", "--qi-url=" + "tcp://" + options.ip + ":" + str(options.port)])
-        tts_event_watcher = SpeechWatcher(app, pub)
     except rospy.ROSInterruptException:
         pass
--- cogrob_ws/src/ros_audio_pkg/src/voice_detection.py
+++ cogrob_ws/src/ros_audio_pkg/src/voice_detection.py
@@ -2,51 +2,78 @@
 import rospy
 from std_msgs.msg import Int16MultiArray, Bool
 import numpy as np
-
+import qi
 import time
+from optparse import OptionParser
 import speech_recognition as sr
 
-pub = rospy.Publisher('mic_data', Int16MultiArray, queue_size=10)
-rospy.init_node('voice_detection_node', anonymous=True)
+class SpeechWatcher:
+    """ A class to react to the ALTextToSpeech/Status event """
 
-should_listen = True
+    def __init__(self, app):
+        app.start()
+        session = app.session
+        self.memory = session.service("ALMemory")
+        
+        self.subscriber_start = self.memory.subscriber("ALTextToSpeech/TextStarted")
+        self.subscriber_done = self.memory.subscriber("ALTextToSpeech/TextDone")
+        #print(dir(type(self.memory)))
+        #self.subscriber = self.memory.subscribeToEvent("ALTextToSpeech/TextDone", "text2speech_node", "on_text_done")
+
+        self.subscriber_start.signal.connect(self.on_text_start)
+        self.subscriber_done.signal.connect(self.on_text_done)
+
+        #self.pub = rospy.Publisher("/listen_start", String, queue_size =3)
+
+    def on_text_start(self, value):
+        self.value_s = value
+        if value == 1:
+            print("start")
+
+    def on_text_done(self, value):
+        self.value_d = value
+        if value == 1:
+            print("done")
 
 # this is called from the background thread
 def callback(recognizer, audio):
-    global should_listen
-    if should_listen:
+    if not tts_event_watcher.value_s or tts_event_watcher.value_d:
+        print("ascolto")
         data = np.frombuffer(audio.get_raw_data(), dtype=np.int16)
         data_to_send = Int16MultiArray()
         data_to_send.data = data
-        
         pub.publish(data_to_send)
 
-def callback_listen(data):
-    global should_listen
-    should_listen = data.data
+if __name__ == "__main__":
+    tts_event_watcher = None
+    rospy.init_node('voice_detection_node', anonymous=True)
+    parser = OptionParser()
+    parser.add_option("--ip", dest="ip", default="10.0.1.207")
+    parser.add_option("--port", dest="port", default=9559)
+    (options, args) = parser.parse_args()
+    app = qi.Application(["SpeechWatcher", "--qi-url=" + "tcp://" + options.ip + ":" + str(options.port)])
+    tts_event_watcher = SpeechWatcher(app)
+    # Initialize a Recognizer
+    r = sr.Recognizer()
 
-# Initialize a Recognizer
-r = sr.Recognizer()
+    #import sounddevice as sd
+    #sd.query_devices()
 
-#import sounddevice as sd
-#sd.query_devices()
+    # Audio source
+    m = sr.Microphone(device_index=6,
+                        sample_rate=16000,
+                        chunk_size=1024)
 
-# Audio source
-m = sr.Microphone(device_index=6,
-                    sample_rate=16000,
-                    chunk_size=1024)
+    # Calibration within the environment
+    # we only need to calibrate once, before we start listening
+    print("Calibrating...")
+    with m as source:
+        r.adjust_for_ambient_noise(source,duration=3)  
+    print("Calibration finished")
 
-# Calibration within the environment
-# we only need to calibrate once, before we start listening
-print("Calibrating...")
-with m as source:
-    r.adjust_for_ambient_noise(source,duration=3)  
-print("Calibration finished")
-
-# start listening in the background
-# `stop_listening` is now a function that, when called, stops background listening
-print("Recording...")
-stop_listening = r.listen_in_background(m, callback)
-rospy.Subscriber("check_listen", Bool, callback_listen)
-
-rospy.spin()
+    # start listening in the background
+    # `stop_listening` is now a function that, when called, stops background listening
+    print("Recording...")
+    stop_listening = r.listen_in_background(m, callback)
+    pub = rospy.Publisher('mic_data', Int16MultiArray, queue_size=10)
+    rospy.spin()

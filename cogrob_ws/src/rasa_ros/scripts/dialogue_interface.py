f2cbfc16bf9b2c7181a5ca7e0480929d7f654747
francesca2311 <f.tirino@studenti.unisa.it>
2023-01-16 09:03:57 +0000

    model

--- cogrob_ws/src/rasa_ros/scripts/dialogue_interface.py
+++ cogrob_ws/src/rasa_ros/scripts/dialogue_interface.py
@@ -77,7 +77,7 @@ class InteractionManager:
     def write_to_rasa_and_answer_aloud(self, phrase: String) -> None:
         """Write to rasa and say aloud its answer"""
         bot_answer = self.write_to_rasa(phrase.data)
-        self.say(bot_answer)
+        self.say(bot_answer.answer)
 
     def is_intent_goodbye(self, phrase: String) -> String:
         """Check if the phrase has the intent to close the conversation"""
